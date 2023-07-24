f55972a94999ce9004ef83fb865e70440a544d99
Monali Valunj <monavalunj96@gmail.com>
2023-07-23 23:07:39 -0700

    started working on script js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let empId = document.getElementById("id");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let ext = document.getElementById("ext");
+let dept = document.getElementById("dept");
