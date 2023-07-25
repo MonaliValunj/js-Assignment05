01a1ec60da4ab3798fd50756fce408b9a73dcc26
Monali Valunj <monavalunj96@gmail.com>
2023-07-25 07:54:14 -0700

    in progress

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
-let empId = document.getElementById("id");
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let ext = document.getElementById("ext");
-let dept = document.getElementById("dept");
+window.addEventListener('load', function(){
+    "use strict"
+    
+
+});
+//Global Variables
+let form;
+let empId;
+let empName; 
+let email;
+let ext;
+let dept;
+let btnSubmit; 
+
+const $ = (id) => document.getElementById(id);
+form = $('empForm');
+empId = $('id');
+empName = $('name');
+email = $('email');
+ext = $('ext');
+dept = $('dept');
+btnSubmit = $('btn-submit');
+
+// console.log(form);
+// console.log(empId);
+// console.log(empName);
+
+
+// function showResult(e){
+//     console.log("Id: "+ empId);
+//     console.log("Name: "+ empName);
+//     console.log("email: "+ email);
+//     console.log("Extension: "+ ext);
+//     e.preventDefault();
+// }
+
+form.addEventListener("onsubmit", function(e){
+    e.preventDefault();
+
+    console.log("Id: "+ empId.value);
+    console.log("Name: "+ empName.value);
+    console.log("email: "+ email.value);
+    console.log("Extension: "+ ext.value);
+
+});
